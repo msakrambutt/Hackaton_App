6d093a94b4f0f2ed7ac67bbe71ccaab123786cf2
unknown <salmanahmed121221@gmail.com>
2023-05-28 21:00:51 +0500

    Make Navbar Footer HeroSection Promotion Section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import Navbar from '@/components/ui/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import Footer from '@/components/ui/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
